150a69611b77bde9fb2c1f568c4cab7dd81fd334
Piotr Witek <piotrek.witek@gmail.com>
2022-06-09 17:07:08 +0200

    non breaking code improvements and tsconfig cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ try {
   // default ts-node config
   const project =
     process.env.TS_NODE_PROJECT ||
-    process.env._TS_PROJECT_PATH__ ||
+    process.env._TS_PROJECT_PATH__ || // deprecated flag
     './tsconfig.json';
   const transpileOnly = !process.env.TS_TYPE_CHECK;
   require('ts-node').register({
