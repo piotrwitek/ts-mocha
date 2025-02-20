bf9627a75643f009a5e9fca388ff32fdf1d23e1b
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-02-20 15:30:09 +0100

    refactor: improve test script commands and add logging to programmatic use tests

--- test/baseline/programmatic-use-test.js
+++ test/baseline/programmatic-use-test.js
@@ -1,14 +1,15 @@
-process.env.TS_NODE_PROJECT = './test/paths/tsconfig.json';
-require('../..');
-const Mocha = require('mocha');
-const path = require('path');
+process.env.TS_NODE_PROJECT = "./test/paths/tsconfig.json";
+require("../..");
+const Mocha = require("mocha");
+const path = require("path");
 
 const mocha = new Mocha();
 mocha.addFile(path.resolve(__dirname, `app.spec.ts`));
-// Add a test with a compile error to prove that type checks are off by default.
-mocha.addFile(path.resolve(__dirname, `../typecheck/app.spec.ts`));
+
+console.log("Programmatic use test start.");
 mocha.run((failures) => {
-  process.on('exit', () => {
+  process.on("exit", () => {
+    console.log("Programmatic use test complete.");
     process.exit(failures); // exit with non-zero status if there were failures
   });
 });
--- test/paths/programmatic-use-test.js
+++ test/paths/programmatic-use-test.js
@@ -1,13 +1,16 @@
-process.env.TS_NODE_PROJECT = './test/paths/tsconfig.json';
+process.env.TS_NODE_PROJECT = "./test/paths/tsconfig.json";
 process.env.TS_CONFIG_PATHS = true;
-require('../..');
-const Mocha = require('mocha');
-const path = require('path');
+require("../..");
+const Mocha = require("mocha");
+const path = require("path");
 
 const mocha = new Mocha();
 mocha.addFile(path.resolve(__dirname, `app.spec.ts`));
+
+console.log("Programmatic use test start.");
 mocha.run((failures) => {
-  process.on('exit', () => {
+  process.on("exit", () => {
+    console.log("Programmatic use test complete.");
     process.exit(failures); // exit with non-zero status if there were failures
   });
 });
